cb5d0cdac12bdedd229d2dea7e8c146451ac2e91
PauloBragaCosta <48026280+PauloBragaCosta@users.noreply.github.com>
2021-01-15 14:35:21 -0300

    Beta 6

--- src/components/insta.tsx
+++ src/components/insta.tsx
@@ -10,7 +10,7 @@ export default function Insta() {
 			<div id="container">
 				<div id="columns">
 					<div id="column-mobile-screenshots">
-						<img id="screenshotinsta" src="https://instagram.fvdc5-1.fna.fbcdn.net/v/t51.2885-15/e35/124290114_185762476419461_9045566085261928527_n.jpg?_nc_ht=instagram.fvdc5-1.fna.fbcdn.net&_nc_cat=105&_nc_ohc=OItDn4GA7AsAX9Mi-mQ&tp=1&oh=fa611c33fe476c3ff0533b47f13ccc66&oe=6029DF5A"/>
+						<img id="screenshotinsta" src="https://instagram.fvdc5-1.fna.fbcdn.net/v/t51.2885-15/e35/124290114_185762476419461_9045566085261928527_n.jpg?_nc_ht=instagram.fvdc5-1.fna.fbcdn.net&_nc_cat=105&_nc_ohc=OItDn4GA7AsAX9Mi-mQ&tp=1&oh=fa611c33fe476c3ff0533b47f13ccc66&oe=6029DF5A" alt=""/>
 					</div>
 					<div id="column-is-two-fifths">
 						<h1>Carimbos automáticos</h1>
